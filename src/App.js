a54da1fc07a4130862bf05b581847aed0a0591c3
Dukas Kanon <lolololokokokorfff@gmail.com>
2022-06-25 15:42:32 +0300

    initialize commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import Converter from './components/Converter';
+import Copyright from './components/Copyright';
+import Header from './components/Header';
+import { fetchData } from './http/fetchCurrencies';
+
+function App() {
+
+  const [rates, setRates] = useState([]);
+
+  useEffect(() => {
+    fetchData().then(response =>  setRates(response.rates));
+  }, [])
+
+  return (
+    <div className='app'>
+      <div className='main'>
+        <Header rates={rates} />
+        <div className='container'>
+          <Converter rates={rates} />
+        </div>
+        <Copyright/>
+      </div>
+    </div>
+  )
+}
+
+export default App;
+
